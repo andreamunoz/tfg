7fe192c2af00de742010bd7d5d5d7b25b857bd05
Roberdg <rodi01@ucm.es>
2018-03-22 19:47:30 +0100

    tablas

---
+++ SQL-Lab/inc/esta_contenido.php
@@ -0,0 +1,47 @@
+<?php
+include_once 'functions.php';
+
+class EstaContenido{
+
+
+    function insertarEjercicioAlaHoja($id_hoja, $id_ejercicio){
+    	// * cambiarlo por nombre de hoja
+        $connect = new Tools();
+        $conexion = $connect->connectDB();
+        $orden = "SELECT max(orden) FROM esta_contenido WHERE id_hoja=$id_hoja;";
+        $orden = $orden + 1;
+    	$sql = "INSERT INTO esta_contenido(id_ejercicio,id_hoja,orden) VALUES ('".$id_ejercicio."','".$id_hoja."','".$orden."');";
+        $consulta = mysqli_query($conexion,$sql);
+        $connect->disconnectDB($conexion);
+        return $consulta;
+    }
+
+    function eliminarEjercicioDeLaHoja($id_hoja, $id_ejercicio){
+
+        $connect = new Tools();
+        $conexion = $connect->connectDB();
+    	$sql = "DELETE FROM esta_contenido WHERE id_hoja=$id_hoja and id_ejercicio=$id_ejercicio;";
+        $consulta = mysqli_query($conexion,$sql);
+        $connect->disconnectDB($conexion);
+        return $consulta;
+    }
+
+	function getAllEjerciciosByHoja($id_hoja){
+
+        $connect = new Tools();
+        $conexion = $connect->connectDB();
+    	$sql = "DELETE FROM esta_contenido WHERE id_hoja=$id_hoja;";
+        $consulta = mysqli_query($conexion,$sql);
+        $connect->disconnectDB($conexion);
+        return $consulta;
+    }
+
+    function getEjerciciosNoHoja($id_hoja){
+
+        $sql = "SELECT * FROM esta_contenido WHERE id_hoja<>$id_hoja;";
+        $tool = new Tools();
+        $array = $tool->getArraySQL($sql);
+        return $array;
+    }
+
+}

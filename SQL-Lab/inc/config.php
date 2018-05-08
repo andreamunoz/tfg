76747736aab339aab0831e3772cf68aeb7180b5f
andreamunoz <andmunoz@ucm.es>
2018-05-08 17:06:41 +0200

    Prueba schema_andrea

--- SQL-Lab/inc/config.php
+++ SQL-Lab/inc/config.php
@@ -2,7 +2,8 @@
 define("SERVER","localhost");
 define("USER","tfg17sql");
 define("PASS","tfg17sql");
-define("DB","tfg17sql");
+define("DB","schema_andrea")
+// define("DB","tfg17sql");
 //OTROS DATOS
 define("DEBUG","true");
 ?> 
